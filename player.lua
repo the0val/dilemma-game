0dcfca53d39fb0e013d54dfce768cb53a6f0824e
David Agardh <agardh.david@gmail.com>
2018-11-30 10:53:57 +0100

    animation now stops when player standing still

--- player.lua
+++ player.lua
@@ -1,7 +1,6 @@
 local animation = require"animation"
-local player = {x = 30, y = 30}
-local speedX, speedY = 0, 0
-local maxspeed = 18
+local player = {x = 30, y = 30, isMoving = false}
+local maxSpeed = 18
 local floor = math.floor
 
 player.motion = animation.newAnimation(love.graphics.newImage("player.png"), 10, 20, 0.4)
@@ -12,23 +11,28 @@ function player.update(dt)
 		player.motion.currentTime = player.motion.currentTime - player.motion.duration
 	end
 	
+	player.isMoving = false
 	if love.keyboard.isDown("w") then
-		player.y = player.y - maxspeed * dt
+		player.y = player.y - maxSpeed * dt
+		player.isMoving = true
 	end
 	if love.keyboard.isDown("s") then
-		player.y = player.y + maxspeed * dt
+		player.y = player.y + maxSpeed * dt
+		player.isMoving = true
 	end
 	if love.keyboard.isDown("a") then
-		player.x = player.x - maxspeed * dt
+		player.x = player.x - maxSpeed * dt
+		player.isMoving = true
 	end
 	if love.keyboard.isDown("d") then
-		player.x = player.x + maxspeed * dt
+		player.x = player.x + maxSpeed * dt
+		player.isMoving = true
 	end
 end
 
 function player.draw()
-	local spriteNum = math.floor(player.motion.currentTime / player.motion.duration * #player.motion.quads) + 1
-	love.graphics.draw(player.motion.spriteSheet, player.motion.quads[spriteNum], floor(player.x + 0.5), floor(player.y + 0.5), 0)
+	local spriteNum = player.isMoving and math.floor(player.motion.currentTime / player.motion.duration * #player.motion.quads) + 1 or 1
+	love.graphics.draw(player.motion.spriteSheet, player.motion.quads[spriteNum], floor(player.x + 0.5), floor(player.y + 0.5))
 end
 
 return player
