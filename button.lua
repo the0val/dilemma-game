74ad86ab5bb28feb5e49faa06bc6577647b9dffb
David Agardh <agardh.david@gmail.com>
2018-12-09 18:44:41 +0100

    rewrite button.lua
    includes knownLists for iterating and checking all enabled buttons

--- button.lua
+++ button.lua
@@ -1,21 +1,46 @@
 local button = {}
+local knownLists = {}
+local selfRef = {}
+local meta = {__index = selfRef}
 
-function button.new(buttonList, x, y, width, height, callback, args)
-	buttonList = {}
-	buttonList.x = x
-	buttonList.y = y
-	buttonList.width = width
-	buttonList.height = height
-	buttonList.callback = callback
-	buttonList.args = args
+
+function button.newButton(x, y, w, h, callback, args)
+	local output = {}
+	output.x = x
+	output.y = y
+	output.width = w
+	output.height = h
+	function output.callback()
+		if args then
+			callback(unpack(args))
+		else
+			callback()
+		end
+	end
+
+	setmetatable(output, meta)
+
+	return output
 end
 
-function button.checkClick(buttonList, xPressed, yPressed)
-	for _, v in pairs(button) do
-		if xPressed >= v.x and xPressed <= v.x + v.width and
-			yPressed >= v.y and yPressed <= v.y + v.height
-		then
-			v.callback(v.args)
+function button.initList(list, initEnabled)
+	table.insert(knownLists, list)
+	list.enabled = initEnabled
+	return list
+end
+
+function selfRef:checkClick(xC, yC)
+	if xC >= self.x and yC >= self.y and xC < self.x+self.width and yC < self.y+self.height then
+		self.callback()
+	end
+end
+
+function button.checkAll(xC, yC)
+	for _, t in ipairs(knownLists) do
+		if t.enabled then
+			for __, v in ipairs(t) do
+				v:checkClick(xC, yC)
+			end
 		end
 	end
 end
